399e09bb7a06c28d62c1e6051bd44585a034a3a1
stquinones <squinones@goiar.com>
2025-03-30 16:37:04 -0300

    agrego cors ii

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
-
+const cors = require('cors')
 app.use(express.json())
 app.use(express.static('dist'))
+app.use(cors())
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
