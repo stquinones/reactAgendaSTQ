3032f0841bcf0be9e1c7d3fa67778ae786798c1e
stquinones <squinones@goiar.com>
2025-03-30 16:51:16 -0300

    arr

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 app.use(express.json())
-app.use(express.static('dist'))
 app.use(cors())
+app.use(express.static('dist'))
+
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
